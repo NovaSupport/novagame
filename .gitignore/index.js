705756638dbf9aa9389b8753877d842a34d36268
NovaSupport <46214967+NovaSupport@users.noreply.github.com>
2019-06-22 16:00:39 +1100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14,25 +14,19 @@ var nom = process.env.NOM;
 var prefix = process.env.PREFIX;
 
 function function1() {
-  console.log("Jeu actualisé en 'Read Roblox Request'.");
-  client.user.setActivity("Read Roblox Request");
-  setTimeout(function2, 5000);
+  console.log("Jeu actualisé en 'Logs'.");
+  client.user.setActivity("Logs");
+  setTimeout(function2, 9000);
 }
 
 function function2() {
-  console.log("Jeu actualisé en 'Logs'.");
-  client.user.setActivity("Logs");
-  setTimeout(function3, 5000);
+  console.log("Jeu actualisé en '/help | The Last PUB'.");
+  client.user.setActivity("/help | The Last PUB");
+  setTimeout(function3, 9000);
 }
 
 function function3() {
-  console.log("Jeu actualisé en '!help | Nova'.");
-  client.user.setActivity("!help | Nova");
-  setTimeout(function4, 5000);
-}
-
-function function4() {
-  console.log("Jeu actualisé en '{🔨} xfirox#6001'.");
-  client.user.setActivity("{🔨} xfirox#6001");
-  setTimeout(function1, 5000);
+  console.log("Jeu actualisé en 'Manage The Last PUB'.");
+  client.user.setActivity("Manage The Last PUB");
+  setTimeout(function1, 9000);
 }
